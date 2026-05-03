a00ebd4ae876583f1bcf574ca3e69295dc51a0a6
seungminlee <seungmin.lee@student.uts.edu.au>
2026-05-03 20:10:55 +1000

    feat: add Xcode project, preview, and README

---
+++ NAVIQ-draft/NAVIQ_draftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAVIQ_draftApp.swift
+//  NAVIQ-draft
+//
+//  Created by 이현지 on 3/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct NAVIQ_draftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
